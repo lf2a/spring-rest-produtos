7b74b797ad9e9473e6bcf7bd0277528511e722fe
Luiz Filipy <luizfilipy.aa@outlook.com>
2021-03-17 14:13:13 -0300

    Criando operação de instanciação

--- src/main/java/com/github/lf2a/SpringProdutosApplication.java
+++ src/main/java/com/github/lf2a/SpringProdutosApplication.java
@@ -1,13 +1,29 @@
 package com.github.lf2a;
 
+import com.github.lf2a.domain.Categoria;
+import com.github.lf2a.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringProdutosApplication {
+public class SpringProdutosApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository repo;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringProdutosApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        repo.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
