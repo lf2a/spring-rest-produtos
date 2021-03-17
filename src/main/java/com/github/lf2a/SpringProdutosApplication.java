30aeb39b97c7ca3ab69c57876ca06339257bb923
Luiz Filipy <luizfilipy.aa@outlook.com>
2021-03-17 12:22:23 -0300

    Projeto criado

---
+++ src/main/java/com/github/lf2a/SpringProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.github.lf2a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProdutosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProdutosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/lf2a/SpringProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lf2a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProdutosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
