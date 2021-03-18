69af26c244045ed3ff0bf9897b73b03837395a8c
Luiz Filipy <luizfilipy.aa@outlook.com>
2021-03-18 09:47:59 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/github/lf2a/resources/CategoriaResource.java
+++ src/main/java/com/github/lf2a/resources/CategoriaResource.java
@@ -4,11 +4,13 @@ import com.github.lf2a.domain.Categoria;
 import com.github.lf2a.dto.CategoriaDTO;
 import com.github.lf2a.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -60,7 +62,7 @@ public class CategoriaResource {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Void> delete(@PathVariable("id") Integer id){
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
         service.delete(id);
 
         return ResponseEntity.noContent().build();
@@ -74,6 +76,19 @@ public class CategoriaResource {
                 .stream()
                 .map(CategoriaDTO::new)
                 .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(catDto);
+    }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+        var categoriaList = service.findPage(page, linesPerPage, orderBy, direction);
+
+        var catDto = categoriaList.map(CategoriaDTO::new);
 
         return ResponseEntity.ok().body(catDto);
     }
--- src/main/java/com/github/lf2a/services/CategoriaService.java
+++ src/main/java/com/github/lf2a/services/CategoriaService.java
@@ -6,6 +6,9 @@ import com.github.lf2a.services.exceptions.DataIntegrityException;
 import com.github.lf2a.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -52,5 +55,11 @@ public class CategoriaService {
 
     public List<Categoria> findAll() {
         return repo.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        return repo.findAll(pageRequest);
     }
 }
